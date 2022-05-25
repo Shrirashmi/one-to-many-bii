13e1183c839b92564944201323c15797cd691af0
Shrirashmi <shrirashmimj@gmail.com>
2022-05-25 22:21:33 +0530

    TestSaveMobile code

--- src/main/java/com/ty/onetomany/controller/TestSaveMobileSim.java
+++ src/main/java/com/ty/onetomany/controller/TestSaveMobileSim.java
@@ -18,31 +18,41 @@ public class TestSaveMobileSim {
 		EntityTransaction entityTransaction = entityManager.getTransaction();
 		
 		Sim sim1 = new Sim();
-		sim1.setProvider("Airtel");
-		sim1.setType("3G");
+		sim1.setProvider("Jio");
+		sim1.setType("4G");
+		sim1.setImei(10);
 		
 		Sim sim2 = new Sim();
-		sim2.setProvider("Jio");
-		sim2.setType("4G");
+		sim2.setProvider("Airtel");
+		sim2.setType("3G");
+		sim2.setImei(81);
 		
 		Sim sim3 = new Sim();
-		sim3.setProvider("Idea");
-		sim3.setType("3G");
+		sim3.setProvider("BSNL");
+		sim3.setType("2G");
+		sim3.setImei(26);
+		
 		List<Sim> sims = new ArrayList<Sim>();
 		sims.add(sim1);
 		sims.add(sim2);
 		
 		Mobile mobile1 = new Mobile();
-		mobile1.setName("Motorola");
-		mobile1.setCost(19000);
+		mobile1.setName("Samsung");
+		mobile1.setCost(16000);
+	
 		
 		mobile1.setSims(sims);
+		
 		sim1.setMobile(mobile1);
 		sim2.setMobile(mobile1);
 		sim3.setMobile(mobile1);
+		
 		entityTransaction.begin();
-		entityManager.persist(sim1);
-		entityManager.persist(sim2);
+//		entityManager.persist(sim1);
+//		entityManager.persist(sim2);
+		
+		/* saving the child will save parent entity as well , need not persist separately */ 
+		
 		entityManager.persist(mobile1);
 		entityTransaction.commit();
 		
